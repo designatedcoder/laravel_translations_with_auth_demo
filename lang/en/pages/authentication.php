c0d45c93392c65b97d8c1da7e60aef91589d0c81
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2023-10-01 18:30:20 -0400

    custom pages translations for register page

---
+++ lang/en/pages/authentication.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Name' => 'Name',
+    'Email' => 'Email',
+    'Password' => 'Password',
+    'Confirm Password' => 'Confirm Password',
+    'Already Registered?' => 'Already Registered?',
+    'Register' => 'Register',
+];
